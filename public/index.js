bc4a3b2270a4423e995945cdec0027a6a5f41062
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-23 04:36:50 +0100

    start script

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const PORT = process.env.PORT || 5000
 
 const app = express();
 
-app.listen(4000);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // middleware and static files
 app.use(express.static('static'));
