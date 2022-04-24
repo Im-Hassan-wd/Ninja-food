468864f816dd6d1e84d57909c44121894dab6da6
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-24 20:49:21 +0100

    MVC

--- controllers/controller.js
+++ controllers/controller.js
@@ -12,3 +12,19 @@ module.exports.home_get = (req, res ) => {
 
     res.render('index', { title: 'home', lists: []});
 }
+
+module.exports.recipe_get = (req, res) => {
+  res.render('recipe',  { title: 'recipe' });
+}
+
+module.exports.create_get = (req, res) => {
+  res.render('create',  { title: 'Add new recipe', lists: ['Create']});
+}
+
+module.exports.create_post = (req, res) => {
+  const recipe = new Recipe(req.body)
+
+  recipe.save()
+   .then(result => res.status(200).json({ recipe: recipe._id }))
+   .catch(err => console.log(err))
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe');
-const { home_get } = require('./controllers/controller')
+const { home_get, recipe_get, create_get, create_post } = require('./controllers/controller')
 const PORT = process.env.PORT || 4000
 
 const app = express();
@@ -26,22 +26,9 @@ mongoose.connect(dbURI)
 
 // routes
 app.get('/', home_get);
-
-app.get('/recipes', (req, res) => {
-  res.render('recipe',  { title: 'recipe' });
-});
-
-app.get('/create', (req, res) => {
-  res.render('create',  { title: 'Add new recipe', lists: ['Create']});
-});
-
-app.post('/create', (req, res) => {
-  const recipe = new Recipe(req.body)
-
-  recipe.save()
-   .then(result => res.status(200).json({ recipe: recipe._id }))
-   .catch(err => console.log(err))
-})
+app.get('/recipes', recipe_get);
+app.get('/create', create_get);
+app.post('/create', create_post)
 
 // 404
 // app.use((req, res) => {
